8d9a498672709fd0bb3d2c7b98041c6687de6f17
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2023-10-27 11:16:36 -0300

    conexão com firebase

--- app.vue
+++ app.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button @click="signIn" class="button is-primary is-rounded">Login</button>
+    <button @click="signOut" class="button is-danger is-rounded">Logout</button>
   </div>
 </template>
+
+<script setup>
+const signIn = async () => {
+  const email = "teste2@email.com";
+  const password = "123456";
+  const credentials = await signInUser(email, password);
+  console.log("Credenciais do Usuario", credentials);
+};
+
+const signOut = async () => {
+  const credentials = await singOutUser();
+  console.log("Credenciais do Usuario", credentials);
+};
+
+onMounted(async () => {
+  /**
+   * Teste Criação de Usuario (ok )
+    console.log("---APP---");
+    const email = "teste3@email.com";
+    const password = "123456";
+    const credentials = await createUser(email, password);
+    console.log("Credenciais do Usuario", credentials);
+  */
+});
+</script>
+
+<script></script>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
