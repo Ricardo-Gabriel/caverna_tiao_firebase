83a22f1261217def7fc3f1a547787dc5de0fb1c4
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2023-10-27 17:46:02 -0300

    firepage not working

--- app.vue
+++ app.vue
@@ -10,4 +10,5 @@ console.log("---!---");
 
 <style>
 @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
 </style>
---
+++ components/AuthFirebase.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2 class="mb-5 title is-4 has-text-black">{{ title }}</h2>
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control has-icons-left has-icons-right">
+        <input
+          class="input"
+          v-model="form.email"
+          type="email"
+          placeholder="exemplo@email.com"
+          autocomplete="false"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+      </div>
+      <div class="field">
+        <label class="label">Senha</label>
+        <div class="control">
+          <input
+            class="input"
+            v-model="form.password"
+            type="password"
+            placeholder="*********"
+            autocomplete="false"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-if="message" class="has-text-sucess p-3">
+      {{ message }}
+    </div>
+    <button class="button is-success" @click="$emit('submit')">Submit</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  form: Object,
+  title: Strinng,
+  message: String,
+});
+</script>
